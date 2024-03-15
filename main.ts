a788a31f37e027a593396a784673155151d4c56e
Ryan <ryan@192.168.124.18>
2024-03-16 00:50:04 +0800

    first testing version for taskmgr

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, normalizePath,DataAdapter, Vault, FileSystemAdapter, TFile } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
@@ -10,72 +10,123 @@ const DEFAULT_SETTINGS: MyPluginSettings = {
 	mySetting: 'default'
 }
 
+class Task{
+	task: string
+	check: boolean
+}
+
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		// const statusBarItemEl = this.addStatusBarItem();
+		// statusBarItemEl.setText('Status Bar Text');
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+		this.registerMarkdownCodeBlockProcessor("task", (source, el, ctx) => {
+			/*
+				```task
+					src "more/task.md"
+					task1
+					task2
+					task3
+					...
+				```
+			*/
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+			const taskfile = "task.md"
+			let file = this.app.vault.getFileByPath(taskfile)
+			if(file === null){return}
+			// {
+			let f = this.app.vault.read(file)
+			let fstring = ""
+			f.then(v => { 
+				// 定义任务识别函数
+				let identifyTasks = (content: string) : Task[] => {
+					const taskPrefix: string = '- [ ] ';
+					const completedPrefix: string = '- [x] ';
+					const tasks: string[] = content.split('\n').filter(line => {
+						return line.startsWith(taskPrefix) || line.startsWith(completedPrefix);
+					});
+
+					return tasks.map(task => {
+						const taskWithoutPrefix: string = task.replace(taskPrefix, '').replace(completedPrefix, '');
+						const isCompleted: boolean = task.startsWith(completedPrefix);
+						return { task: taskWithoutPrefix, check: isCompleted };
+					});
 				}
-			}
-		});
+
+				// 比较两个任务数组并更新 taskNew 的 check 属性
+				const updateTaskChecks = (taskOld: Task[], taskNew: Task[]): void => {
+					for (const task of taskOld) {
+						for (const newTask of taskNew) {
+							if (newTask.task === task.task) {
+								newTask.check = task.check;
+							}
+						}
+					}
+				}
+				
+
+				// 获取任务对象
+				const tasksOld: Task[] = identifyTasks(v);
+				let tasksNew = source.split('\n').filter(line => line.trim() !== "").map(task => {
+					return {task: task, check: false}
+				})
+				if(!tasksNew){return}
+
+				updateTaskChecks(tasksOld, tasksNew)
+				tasksNew.sort((a, b) => {
+					// 如果 a 的 check 属性为 false，而 b 的 check 属性为 true，返回 -1 以将 a 排在前面
+					if (a.check === false && b.check === true) {
+						return -1;
+					}
+					// 如果 a 的 check 属性为 true，而 b 的 check 属性为 false，返回 1 以将 b 排在前面
+					if (a.check === true && b.check === false) {
+						return 1;
+					}
+					// 其他情况返回 0，保持原来的相对顺序
+					return 0;
+				})
+				
+				const planttext =  tasksNew.map(task => `- ${task.check? '[x]' : '[ ]'} ${task.task}`).join("\n");
+				
+				// console.log(planttext)
+				this.app.vault.adapter.write(taskfile, planttext)
+			})
+
+
+			//============================
+			// const rows = source.split("\n").filter((row) => row.length > 0);
+	  
+			// const table = el.createEl("table");
+			// const body = table.createEl("tbody");
+	  
+			// for (let i = 0; i < rows.length; i++) {
+			//   const cols = rows[i].split(",");
+	  
+			//   const row = body.createEl("tr");
+	  
+			//   for (let j = 0; j < cols.length; j++) {
+			// 	row.createEl("td", { text: cols[j] });
+			//   }
+			// }
+		  });
+
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+		// this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
+		// 	console.log('click', evt);
+		// });
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		// this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -91,21 +142,6 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
 
 class SampleSettingTab extends PluginSettingTab {
 	plugin: MyPlugin;
@@ -120,15 +156,15 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		// new Setting(containerEl)
+		// 	.setName('Setting #1')
+		// 	.setDesc('It\'s a secret')
+		// 	.addText(text => text
+		// 		.setPlaceholder('Enter your secret')
+		// 		.setValue(this.plugin.settings.mySetting)
+		// 		.onChange(async (value) => {
+		// 			this.plugin.settings.mySetting = value;
+		// 			await this.plugin.saveSettings();
+		// 		}));
 	}
 }
